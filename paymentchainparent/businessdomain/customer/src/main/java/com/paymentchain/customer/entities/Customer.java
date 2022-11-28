073cca171c3473b1ad01f32a41e0664bab74c9b3
Eduardo Vicente <evicente.ti@gmail.com>
2022-11-27 23:30:38 -0500

    Video 31 - 27/11/2022 23:30

--- paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
+++ paymentchainparent/businessdomain/customer/src/main/java/com/paymentchain/customer/entities/Customer.java
@@ -5,31 +5,36 @@
  */
 package com.paymentchain.customer.entities;
 
-import javax.persistence.*;
-
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Transient;
 import lombok.Data;
 
-import java.util.List;
-
 /**
+ *
  * @author sotobotero
  */
 @Entity
 @Data
 public class Customer {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String code;
-    private String name;
-    private String phone;
+   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+   private long id;
+   private String code;
+   private String name;
+   private String phone;   
     private String iban;
-    private String surname;
-    private String address;
-
-    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)
+    private String surname; 
+    private String address;     
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "customer", cascade = CascadeType.ALL, orphanRemoval = true)   
     private List<CustomerProduct> products;
-
     @Transient
     private List<?> transactions;
+   
 }
--- paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/entities/Product.java
+++ paymentchainparent/businessdomain/product/src/main/java/com/paymentchain/product/entities/Product.java
@@ -1,19 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.paymentchain.product.entities;
-
-import lombok.Data;
-import lombok.Generated;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.Data;
 
+/**
+ *
+ * @author sotobotero
+ */
 @Entity
 @Data
 public class Product {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private long id;
-    private String code;
-    private String name;
+   @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+   private long id;
+   private String code;
+   private String name;
+   
 }
